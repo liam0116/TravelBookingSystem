5f1589c1d29ff9e58f451e0c302eb5d5a3356f94
liam <liam460116@gmail.com>
2023-11-16 10:37:51 +0800

    fix:增加計算排名最高的旅宿

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// Controllers
+use App\Http\Controllers\BnbsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/*
+* restful api 规范
+*/
+Route::get('/Bnbs/top', [BnbsController::class, 'getTopBnbs']);
